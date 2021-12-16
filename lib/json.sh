ed0c058c840e059e5fb05f6728aa8e42c0bed83b
Jason Kelly <jason-kelly@live.dk>
2021-12-16 10:59:31 +0100

    Fix: read_json function not present

--- lib/json.sh
+++ lib/json.sh
@@ -10,3 +10,19 @@ json_get_key() {
     echo ""
   fi
 }
+
+read_json() {
+  local file="$1"
+  local key="$2"
+
+  if test -f "$file"; then
+    # -c = print on only one line
+    # -M = strip any color
+    # --raw-output = if the filter’s result is a string then it will be written directly
+    #                to stdout rather than being formatted as a JSON string with quotes
+    # shellcheck disable=SC2002
+    cat "$file" | $JQ -c -M --raw-output "$key // \"\"" || return 1
+  else
+    echo ""
+  fi
+}
